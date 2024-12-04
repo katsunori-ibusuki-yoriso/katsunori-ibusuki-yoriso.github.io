74626535bc626c45afdad8218d6b72cdd5482c56
katsunori-ibusuki-yoriso <katsunori-ibusuki@yoriso.com>
2024-12-04 09:54:28 +0900

    ビルド修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,25 @@
 export default defineNuxtConfig({
   app: {
     baseURL: "katsunori-ibusuki-yoriso.github.io",
+    buildAssetsDir: ".nuxt/",
   },
-
-  ssr: true,
   nitro: {
-    preset: "github-pages",
+    preset: "static",
+    prerender: {
+      failOnError: false,
+      ignore: [
+        "/katsunori-ibusuki-yoriso.github.io",
+        "/katsunori-ibusuki-yoriso.github.io/404.html",
+        "/katsunori-ibusuki-yoriso.github.io/200.html",
+      ],
+    },
+  },
+  experimental: {
+    payloadExtraction: false,
+  },
+  router: {
+    options: {
+      strict: false,
+    },
   },
 });
